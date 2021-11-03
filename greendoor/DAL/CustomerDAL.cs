3176fb58374c4d7f5c4e2c0b7fb7fadb265636ad
TeoWeiShan <S10205156@connect.np.edu.sg>
2021-11-03 12:23:15 +0800

    Push Project

---
+++ greendoor/DAL/CustomerDAL.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using System.Data.SqlClient;
+using greendoor.Models;
+
+namespace greendoor.DAL
+{
+    public class CustomerDAL
+    {
+        private IConfiguration Configuration { get; }
+        private SqlConnection conn;
+        //Constructor
+        public CustomerDAL()
+        {
+            //Read ConnectionString from appsettings.json file
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json");
+            Configuration = builder.Build();
+            string strConn = Configuration.GetConnectionString(
+            "NPBookConnectionString");
+            //Instantiate a SqlConnection object with the
+            //Connection String read.
+            conn = new SqlConnection(strConn);
+        }
+
+        public int Add(Customer customer)
+        {
+            //Create a SqlCommand object from connection object
+            SqlCommand cmd = conn.CreateCommand();
+            //Specify an INSERT SQL statement which will
+            //return the auto-generated StaffID after insertion
+            cmd.CommandText = @"INSERT INTO Competitor (CompetitorName, Salutation, EmailAddr, Password)
+                                OUTPUT INSERTED.CompetitorID
+                                VALUES(@name, @salutation, @email, @password)";
+            //Define the parameters used in SQL statement, value for each parameter
+            //is retrieved from respective class's property.
+            cmd.Parameters.AddWithValue("@name", customer.CustomerName);
+            cmd.Parameters.AddWithValue("@salutation", customer.Salutation);
+            cmd.Parameters.AddWithValue("@email", customer.EmailAddr);
+            cmd.Parameters.AddWithValue("@password", customer.Password);
+            //A connection to database must be opened before any operations made.
+            conn.Open();
+            //ExecuteScalar is used to retrieve the auto-generated
+            //StaffID after executing the INSERT SQL statement
+            customer.CustomerID = (int)cmd.ExecuteScalar();
+            //A connection should be closed after operations.
+            conn.Close();
+            //Return id when no error occurs.
+            return customer.CustomerID;
+        }
+    }
+}
