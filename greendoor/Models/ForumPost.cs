39c64fa372e703e25495251b8396b30d6d4e801c
j6ine <65351853+j6ine@users.noreply.github.com>
2021-11-11 23:32:02 +0800

    Created forum view. Has error getting data from database.

---
+++ greendoor/Models/ForumPost.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace greendoor.Models
+{
+    public class ForumPost
+    {
+        public int CustomerID { get; set; }
+        public int ForumPostID { get; set; }
+        public string PostName { get; set; }
+        public string PostDescription { get; set; }
+        public DateTime DateTimePosted { get; set; }
+
+    }
+}
