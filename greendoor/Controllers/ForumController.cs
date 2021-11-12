aa7136bac61f16b180bcb6b4948f34893823477c
j6ine <65351853+j6ine@users.noreply.github.com>
2021-11-12 11:26:30 +0800

    Forum post connected to database. Comment section not done yet. View model also has not been created.

--- greendoor/Controllers/ForumController.cs
+++ greendoor/Controllers/ForumController.cs
@@ -1,16 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using greendoor.DAL;
+using greendoor.Models;
 
 namespace greendoor.Controllers
 {
     public class ForumController : Controller
     {
+        private ForumPostDAL forumPostContext = new ForumPostDAL();
+
         public IActionResult Index()
         {
-            return View();
+            List<ForumPost> forumPostList = forumPostContext.GetAllForumPost();
+            return View(forumPostList);
         }
     }
 }
+
